70c663151129509612d1a866eda772192779dc81
Will <doggy.huang@gmail.com>
2024-01-31 09:46:09 +0800

    初始化 ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,32 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideNoopAnimations()
+    provideNoopAnimations(),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+        defaultLanguage: 'zh-tw',
+      })
+    ),
   ]
 };
